373cecc619f3c31a0e9d05d6de7a391f68b49b5b
DavidAlvarezRios <dalvarezrios@gmail.com>
2019-08-18 11:57:06 +0200

    Lee comandos de memoria

--- chip8.c
+++ chip8.c
@@ -87,8 +87,164 @@ int main(int argc, char** argv){
         uint8_t y = (opcode >> 4) & 0x000F;
 
         uint8_t patata = (opcode >> 12);
-
         
+        switch (patata) {
+            
+            case 0:
+                if(opcode == 0x00E0){
+                    printf("CLS");
+                }else if(opcode == 0x00EE){
+                    printf("RET");
+                }
+
+                break;
+            
+            case 1:
+                printf("JP %x", nnn);
+                break;
+            
+            case 2:
+                printf("CALL %x", nnn);
+                break;
+
+            case 3:
+                printf("SE V%x, %x", x, kk);
+                break;
+
+            case 4:
+                printf("SNE V%x, %x", x, kk);
+                break;
+
+            case 5:
+                printf("SE V%x, V%x", x, y);
+                break;
+
+            case 6:
+                printf("LD V%x, %x", x, kk);
+                break;
+
+            case 7:
+                printf("ADD V%x, %x", x, kk);
+                break;
+
+            case 8:
+                
+                switch (n) {
+                    
+                    case 0:
+                        printf("LD V%x, V%x", x, y);
+                        break;
+
+                    case 1:
+                        printf("OR V%x, V%x", x, y);
+                        break;
+               
+               
+                    case 2:
+                        printf("AND V%x, V%x", x, y);
+                        break;
+               
+               
+                    case 3:
+                        printf("XOR V%x, V%x", x, y);
+                        break;
+               
+               
+                    case 4:
+                        printf("ADD V%x, V%x", x, y);
+                        break;
+                    
+                    
+                    case 5:
+                        printf("SUB V%x, V%x", x, y);
+                        break;
+               
+
+                    case 6:
+                        printf("SHR V%x, V%x", x, y);
+                        break;
+               
+
+                    case 7:
+                        printf("SUBN V%x, V%x", x, y);
+                        break;
+               
+                    case 0XE:
+                        printf("SHL V%x, V%x", x, y);
+                        break;
+                                              
+                                 
+               }
+                
+                break;
+               
+            case 0XA:
+                printf("LD I, %x", nnn);
+                break;
+
+            case 0xB:
+                printf("JP V0, %x", nnn);
+                break;
+
+            case 0xC:
+                printf("RND V%x, %x", x, kk);
+                break;
+
+            case 0xD:
+                printf("DRW V%x, V%x, %x", x, y, n);
+                break;
+
+            case 0xE:
+                if( kk == 95 ){
+                    printf("SKP V%x", x);
+                }else if( kk == 0xA1 ){
+                    printf("SKNP V%x", x);
+                }
+
+                break;
+
+            case 0xF:
+                switch (kk) {
+                    
+                    case 0x07:
+                        printf("LD V%x, DT", x);
+                        break;
+                    case 0x0A:
+                        printf("LD V%x, k", x);
+                        break;
+                    case 0x15:
+                        printf("LD DT, V%x", x);
+                        break;
+                    case 0x18:
+                        printf("LD ST, V%x", x);
+                        break;
+                    case 0x1E:
+                        printf("ADD I, V%x", x);
+                        break;
+                    case 0x29:
+                        printf("LD F, V%x", x);
+                        break;
+                    case 0x33:
+                        printf("LD B, V%x", x);
+                        break;
+                    case 0x55:
+                        printf("LD I, V%x", x);
+                        break;
+                    case 0x65:
+                        printf("LD V%x, I", x);
+                        break;
+                    
+                }
+                break;
+
+            default:
+                printf("pene");
+                break;
+
+
+        }
+
+        printf("\n");
         // Control
 
 
